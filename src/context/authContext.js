0cabad68c2c086859ee76b23042194b19279f0dc
Marques Jordan <marques.jordan81@gmail.com>
2022-06-10 01:24:50 -0700

    setup

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -28,20 +28,41 @@ const logout = (dispatch) => {
 const registerUser = (dispatch) => {
   return async ({ email, username, password, confirmPassword }) => {
     try {
-      const response = await api.post(
-        'https://vast-beach-48711.herokuapp.com/api/register',
-        {
+      fetch('https://vast-beach-48711.herokuapp.com/api/register', {
+        mode: 'cors',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
           email: email,
           username: username,
           password: password,
           confirmPassword: confirmPassword,
-        },
-      );
+        }),
+      })
+        .then((response) => response.json())
+        //Then with the data from the response in JSON...
+        .then((data) => {
+          console.log('Success:', data);
+        })
+        //Then with the error genereted...
+        .catch((error) => {
+          console.error('Error:', error);
+        });
 
-      console.log('REPSONSE ', response);
+      // const response = await api.post(
+      //   'https://vast-beach-48711.herokuapp.com/api/register',
+      //   {
+      //     email: email,
+      //     username: username,
+      //     password: password,
+      //     confirmPassword: confirmPassword,
+      //   },
+      // );
 
       // await AsyncStorage.setItem('token', response.data.token);
-      dispatch({ type: 'auth', payload: response.data.token });
+      // dispatch({ type: 'auth', payload: response.data.token });
     } catch (err) {
       dispatch({ type: 'add_error', payload: 'Sign Up Error' });
     }
