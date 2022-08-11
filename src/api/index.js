dea28492b6900c4ec581a8fb4b38c6e978385750
Marques Jordan <marques.jordan81@gmail.com>
2022-08-11 14:17:54 -0700

    Update Proxy

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const URI =
   process.env.NODE_ENV === 'production'
-    ? 'https://appapiproxy.herokuapp.com/https://vast-beach-48711.herokuapp.com'
+    ? 'https://vast-beach-48711.herokuapp.com'
     : 'http://localhost:5000';
 
 export default axios.create({
